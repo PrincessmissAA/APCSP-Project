844348819e3f1ce6e55b14b5b05b10828a9ee13f
PrincessmissAA <73598180+PrincessmissAA@users.noreply.github.com>
2021-04-25 18:10:36 -0600

    Finished card class

--- Class.js
+++ Class.js
@@ -1,4 +1,38 @@
-class Deck {
-    const {Hearts, Spades, Clubs, Diamonds}
-    const {Aces, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King}
+class card{
+    constructor(suit, faceValue, value) {
+        this.suit = suit;
+        this.faceValue = faceValue;
+        this.value = value;
+        if(value > 10 ){
+            this.blackJackValue = 10;
+        }else{
+            this.blackJackValue = value;
+        }
+        this.cardPlayed = 0;
+    }
+
+    getSuit(){
+        return this.suit;
+    };
+    getFaceValue(){
+        return this.faceValue;
+    };
+    getBlackJackValue(){
+        return this.blackJackValue;
+    };
+    getValue(){
+        return this.value;
+    };
+    getCardPlayed(){
+        return this.cardPlayed;
+    };
+
+}
+
+
+class deck {
+
+    const suits = {'Hearts', 'Spades', 'Clubs', 'Diamonds'}
+    const faceValue = {'Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'}
+    const value = 1..13;
 }
