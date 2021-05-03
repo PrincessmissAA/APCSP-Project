f266f56dd5b4bb243e9f6cdc9c32b216a1cd466a
PrincessmissAA <73598180+PrincessmissAA@users.noreply.github.com>
2021-05-03 13:15:10 -0600

    Worked on cards

--- App.js
+++ App.js
@@ -3,10 +3,20 @@ var hitMe = document.createElement("button");
 var stay = document.createElement("button");
 var shuffle = document.createElement("button");
 var nextRound = document.createElement("button");
-var cardBack = back.jpg;
+//var cardBack = back.jpg;
 makeDealerTable();
 //Background Image
 function makeDealerTable(){
     document.body.style.backgroundColor = "green";
 
 }
+
+function cardButton(){
+    this.card.createElement("button");
+    this.card.style.position = "absolute";
+    this.card.style.backgroundImage = card;
+    this.card.style.width = ;
+    this.card.style.height = ;
+}
+
+addEventListener.cardButton("click", alert("Your card value is" + blackJackValue));
