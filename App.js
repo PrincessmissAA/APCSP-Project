94aab7fff78ea632e2fd238e03061a773107c31e
PrincessmissAA <73598180+PrincessmissAA@users.noreply.github.com>
2021-05-17 22:07:31 -0600

    Finished dealCards

--- App.js
+++ App.js
@@ -1,43 +1,73 @@
 var d = new deck();
+var cardArray = d.getDeck();
+
 var dealInput = document.createElement("Input");
+var playerInput = document.createElement("Input");
+var playerButton = document.createElement("button");
 var deal = document.createElement("button");
 var hitMe = document.createElement("button");
 var stay = document.createElement("button");
 var shuffle = document.createElement("button");
 var nextRound = document.createElement("button");
-var cardArray = d.getDeck();
-var dealtHolder = document.createElement("div");
-document.body.append(dealtHolder);
-dealtHolder.innerHTML = "";
 
+var dealtHolderplayer = document.getElementById("playerone")
+var dealtHolderdealer = document.getElementById("playertwo")
 
 makeButtonElements();
-dealCards(dealInput.value);//put dealInput.value here
+dealCards(dealInput.value);
 
-function dealCards(f){
-    //console.log(f);
-    var f = Number(dealInput.value);
+function dealCards(f) {
+    shuffleCards();
+    var f = Number(dealInput.value) * 2;
     var dealCardArray = [];
-    for(var i = 0; i < f; i++){
-    
-        dealCardArray.push(cardArray[i]);
-        var b = dealCardArray[i].cardButton(dealCardArray[i]);
-        b.style.position = "absolute";
-        b.style.left = i*50;
-        b.style.top = 50;
- 
-        dealtHolder.append(b);
+
+    for (var i = 0; i < f; i++) {
+        if (i % 2 == 0) {
+            dealCardArray.push(cardArray[i]);
+            var b = dealCardArray[i].cardButton(dealCardArray[i]);
+            b.style.position = "relative";
+            dealtHolderplayer.append(b);
+        } else {
+            dealCardArray.push(cardArray[i]);
+            var c = dealCardArray[i].cardButton(dealCardArray[i]);
+            c.style.position = "relative";
+            dealtHolderdealer.append(c);
+        }
     }
 }
 
-function makeButtonElements(){
+
+function shuffleCards() {
+    for (var i = 0; i < cardArray.length; i++) {
+        var firstCard = i;
+        var secondCard = 0;
+        firstCard = Math.floor(Math.random() * cardArray.length);
+        secondCard = Math.floor(Math.random() * cardArray.length);
+        if (typeof cardArray[firstCard] != 'undefined' | typeof cardArray[secondCard] != 'undefined') {
+            swapCards(firstCard, secondCard);
+        } else {
+            console.log("undefined pre swap: " + firstCard + "--" + secondCard);
+        }
+        if (typeof cardArray[firstCard] == 'undefined' | typeof cardArray[secondCard] == 'undefined') {
+            console.log("undefined post swap: " + firstCard + "--" + secondCard);
+        }
+    }
+}
+
+
+function swapCards(c1, c2) {
+    var temp = cardArray[c1];
+    cardArray[c1] = cardArray[c2];
+    cardArray[c2] = temp;
+}
+
+function makeButtonElements() {
+    dealInput.placeholder = "number of cards to deal";
+    document.body.prepend(dealInput);
+
     deal.innerHTML = "deal";
     deal.style.margin = "3px";
-    document.body.append(deal);
+    document.body.prepend(deal);
     deal.addEventListener("click", dealCards);
 
-    dealInput.placeholder = "enter dealSize";
-    document.body.append(dealInput);
 }
-//var d = new deck();
-//console.log(d[0].cardImage);
